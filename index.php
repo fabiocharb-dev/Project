8445f02bc4665c92e18c6b6fb2ef7320eaa11545
Ichiko <fabiocharb@gmail.com>
2025-11-22 12:27:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/index.css">
     <title>Accueil_Portfolio</title>
 </head>
 <body>
     
 </body>
+
 </html>
